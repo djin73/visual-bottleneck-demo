60b989ef4b55eb9a4f0fa746205e1c9effe4d47b
Daniel Jin <danieljin73@gmail.com>
2022-08-01 16:17:40 -0700

    add slider

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const LINE_CANVAS_PADDING = 20;
 
 $(document).ready(function () {
-  //TODO change this to load multiple datasets
   const query_string = window.location.search;
   const url_params = new URLSearchParams(query_string);
   let dataset_name = url_params.get("dataset");
@@ -46,6 +45,7 @@ class Visualizer {
     this.ground_truth_card_template = $.templates("#ground-truth-card");
     this.classes_card_template = $.templates("#classes-card");
     this.concept_details_template = $.templates("#concept-details-card");
+    this.slider_template = $.templates("#slider");
   }
 
   visualize(cur_class_id, dataset) {
@@ -82,90 +82,7 @@ class Visualizer {
       $("#middle-inner").html("");
       $("#left-line-canvas").attr("style", "display: none");
     }
-
-    // // If the previous datapoint is present, visualize the parent
-    // if (datapoint.parent_datapoint != null) {
-    //   this.visualize_parent(datapoint.parent_datapoint, dataset);
-    // // } else {
-    // $("#left-inner").html("");
-    // $("#left-line-canvas").attr("style", "display: none");
-    // }
   }
-  /*
-  visualize_parent(parent_datapoint,  dataset) {
-    // Open the left canvas
-    $("#left-line-canvas").attr("style", "display: block");
-
-    // Clear the left
-    $("#left-inner").html("");
-
-    // Render the left
-    let data = dataset.get_card_data(parent_datapoint, "left");
-    $("#left-inner").html(this.history_card_template.render(data));
-
-    // Setup on click callback
-    $(`#left-card`).click(() => {
-      this.visualize(parent_datapoint, dataset);
-    });
-
-    // Setup on scroll callback
-    let draw = () => {
-      let svg_elem = $("#left-line-canvas");
-      let svg_x = svg_elem.position().left;
-      let svg_y = svg_elem.position().top;
-
-      // Get d3 svg and clear the canvas
-      let svg = d3.select("#left-line-canvas");
-      svg.selectAll("*").remove();
-
-      // Get middle element position
-      let middle_elem = $("#middle-card .card-left-anchor");
-      let middle_elem_position = middle_elem.position();
-      let middle_x =
-        middle_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
-      let middle_y = middle_elem_position.top - svg_y + 6;
-
-      // Get left element position
-      let left_elem = $("#left-card .card-section-caption-anchor-circle");
-      let left_elem_position = left_elem.position();
-      let left_x = left_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
-      let left_y = left_elem_position.top - svg_y + 6;
-
-      // Draw line
-      svg
-        .append("line")
-        .style("stroke", "rgba(0, 0, 0, 0.5)")
-        .style("stroke-width", 2)
-        .attr("x1", left_x)
-        .attr("y1", left_y)
-        .attr("x2", middle_x)
-        .attr("y2", middle_y);
-
-      // If there is one more level of parent, draw an additional line from left of the screen
-      let base_svg = d3.select("#base-canvas");
-      base_svg.selectAll("*").remove();
-      if (parent_datapoint.parent_datapoint != null) {
-        base_svg.selectAll("*").remove();
-        let edge_x = 0;
-        let left_card = $("#left-card");
-        let left_card_left_x = left_card.position().left + 1;
-        let left_card_middle_y =
-          left_card.position().top + left_card.outerHeight() / 2 + 10;
-        base_svg
-          .append("line")
-          .style("stroke", "rgba(0, 0, 0, 0.2)")
-          .style("stroke-width", 1)
-          .attr("x1", edge_x)
-          .attr("y1", left_card_middle_y)
-          .attr("x2", left_card_left_x)
-          .attr("y2", left_card_middle_y);
-      }
-    };
-    $(window).resize(draw);
-    $("#middle").scroll(draw);
-    draw();
-  }
-*/
 
   visualize_class(class_id, dataset) {
     // Open the middle canvas
@@ -177,7 +94,7 @@ class Visualizer {
     window.history.replaceState({ path: new_url.href }, "", new_url.href);
 
     // Highlight the step in the left
-    $(`#left-category-${class_id}`) //TODo was middle-
+    $(`#left-category-${class_id}`)
       .addClass("active")
       .siblings()
       .removeClass("active");
@@ -185,7 +102,21 @@ class Visualizer {
     // Clear the middle
     $("#middle-inner").html("");
 
-    const cur_class_data = dataset.classes_data[`${class_id}`]; //TODO new
+    const cur_class_data = dataset.classes_data[`${class_id}`];
+    const concepts = cur_class_data["concepts"];
+
+    // render and set up slider
+    $("#middle-inner").append(
+      this.slider_template.render({
+        slider_value: concepts.length,
+        max_value: concepts.length,
+      })
+    );
+    $("#concepts-slider").change(() => {
+      this.render_concepts(class_id, $("#concepts-slider").val(), dataset);
+      $("#num-concepts-shown").html($("#concepts-slider").val());
+    });
+
     // add ground truth card
     let cls_name = cur_class_data["name"];
     $("#middle-inner").append(
@@ -198,78 +129,9 @@ class Visualizer {
       })
     );
 
-    // For each concept, render the concept card
-    const concepts = cur_class_data["concepts"];
-    concepts.forEach(([concept_id]) => {
-      let data = dataset.get_concept_card_data(
-        concept_id,
-        `middle-${concept_id}`
-      );
-      $("#middle-inner").append(this.concept_card_template.render(data));
-      if (
-        dataset.concept_to_prior &&
-        dataset.concept_to_prior[concept_id] === parseInt(class_id)
-      )
-        $(`#concept-label-${concept_id}`).css("color", "red");
+    $("#middle-inner").append(`<div id="concepts-container"></div>`);
 
-      // set up callbacks for each concept detail card
-      $(`#middle-${concept_id}-card`).click(() => {
-        this.visualize_concept(concept_id, class_id, dataset);
-      });
-    });
-
-    // Setup a scroll callback
-    let draw = () => {
-      let svg_elem = $("#left-line-canvas");
-      let svg_x = svg_elem.position().left;
-      let svg_y = svg_elem.position().top;
-
-      // Get d3 svg and clear the canvas
-      let svg = d3.select("#left-line-canvas");
-      svg.selectAll("*").remove();
-
-      // Get left element
-      let left_elem = $(
-        `#left-category-${class_id} .card-section-caption-anchor-circle`
-      );
-      let left_elem_position = left_elem.position();
-      let left_x = left_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
-      let left_y = left_elem_position.top - svg_y + 6;
-
-      // draw lines to each concept
-      concepts.forEach(([concept_id, wt]) => {
-        let middle_elem = $(`#middle-${concept_id}-card .card-left-anchor`);
-        let middle_elem_position = middle_elem.position();
-        let middle_x =
-          middle_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
-        let middle_y = middle_elem_position.top - svg_y + 6;
-
-        // Draw line
-        // svg
-        //   .append("line")
-        //   .style("stroke", "red")
-        //   .style("stroke-width", 2)
-        //   .attr("x1", middle_x)
-        //   .attr("y1", middle_y)
-        //   .attr("x2", right_x)
-        //   .attr("y2", right_y);
-
-        // Draw a text
-        let text_y = middle_y > left_y ? middle_y + 20 : middle_y - 10;
-        svg
-          .append("text")
-          .text(`${wt}`.substring(0, 7))
-          .style("font-size", "18px")
-          .style("font-weight", "bold")
-          .style("fill", "red")
-          .attr("x", middle_x - 85)
-          .attr("y", text_y);
-      });
-    };
-    $(window).resize(draw);
-    $("#middle").scroll(draw);
-    $("#left").scroll(draw);
-    draw();
+    this.render_concepts(class_id, concepts.length, dataset);
   }
 
   visualize_concept(concept_id, class_id, dataset) {
@@ -303,6 +165,85 @@ class Visualizer {
         ),
       })
     );
+  }
+
+  render_concepts(class_id, num_concepts, dataset) {
+    $("#concepts-container").html("");
+
+    const cur_class_data = dataset.classes_data[`${class_id}`]; //TODO new
+    const concepts_sliced = cur_class_data["concepts"].slice(0, num_concepts);
+
+    // For each concept, render the concept card
+    concepts_sliced.forEach(([concept_id]) => {
+      let data = dataset.get_concept_card_data(
+        concept_id,
+        `middle-${concept_id}`
+      );
+      $("#concepts-container").append(this.concept_card_template.render(data));
+      if (
+        dataset.concept_to_prior &&
+        dataset.concept_to_prior[concept_id] === parseInt(class_id)
+      )
+        $(`#concept-label-${concept_id}`).css("color", "red");
+
+      // set up callbacks for each concept detail card
+      $(`#middle-${concept_id}-card`).click(() => {
+        this.visualize_concept(concept_id, class_id, dataset);
+      });
+    });
+
+    // Setup a scroll callback
+    let draw = () => {
+      let svg_elem = $("#left-line-canvas");
+      let svg_x = svg_elem.position().left;
+      let svg_y = svg_elem.position().top;
+
+      // Get d3 svg and clear the canvas
+      let svg = d3.select("#left-line-canvas");
+      svg.selectAll("*").remove();
+
+      // Get left element
+      let left_elem = $(
+        `#left-category-${class_id} .card-section-caption-anchor-circle`
+      );
+      let left_elem_position = left_elem.position();
+      let left_x = left_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
+      let left_y = left_elem_position.top - svg_y + 6;
+
+      // draw lines to each concept
+      concepts_sliced.forEach(([concept_id, wt]) => {
+        let middle_elem = $(`#middle-${concept_id}-card .card-left-anchor`);
+        let middle_elem_position = middle_elem.position();
+        let middle_x =
+          middle_elem_position.left - svg_x + 6 + LINE_CANVAS_PADDING;
+        let middle_y = middle_elem_position.top - svg_y + 6;
+
+        // Draw line
+        // svg
+        //   .append("line")
+        //   .style("stroke", "red")
+        //   .style("stroke-width", 2)
+        //   .attr("x1", middle_x)
+        //   .attr("y1", middle_y)
+        //   .attr("x2", right_x)
+        //   .attr("y2", right_y);
+
+        // Draw a text
+        let text_y = middle_y > left_y ? middle_y + 20 : middle_y - 10;
+        svg
+          .append("text")
+          .text(`${wt}`.substring(0, 7))
+          .style("font-size", "18px")
+          .style("font-weight", "bold")
+          .style("fill", "red")
+          .attr("x", middle_x - 85)
+          .attr("y", text_y);
+      });
+    };
+    $(window).resize(draw);
+    $("#middle").scroll(draw);
+    $("#left").scroll(draw);
+    draw();
   }
 }
 
